eccb4dc625f68323b6d2cd9d49627445f795a9b4
chiyomomo56562 <kkc7130coding@gmail.com>
2026-04-29 09:51:00 +0900

    feat(0-mvp): step 6 - app-bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './app/App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <div>Hello World</div>
-  </React.StrictMode>
-);
+async function enableMocking() {
+  if (import.meta.env.MODE !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser');
+  return worker.start({
+    onUnhandledRequest: 'bypass',
+  });
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
